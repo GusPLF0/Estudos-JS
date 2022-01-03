9911b844674e191888d4281b0c104b2b9b359d8b
Gustavo Pedro Lima <gpedr56@gmail.com>
2022-01-03 20:10:41 -0300

    Update Revisando.js

--- JS/JSPOO/Objetos_Protoypes/Revisando.js
+++ JS/JSPOO/Objetos_Protoypes/Revisando.js
@@ -1,13 +1,18 @@
+// FORMA ALTERNATIVA PARA CRIAÇÃO DE OBJETOS: 
+
 // const pessoa1 = new Object();
 // pessoa1.nome = 'Gustavo';
 // pessoa1.sobrenome = 'Lima';
 
 const pessoa = {
     nome:'Gustavo',
-    sobrenome:'Lima'
+    sobrenome:'Lima',
+    falarNome: function(){
+        return (`Olá ${this.nome}`)
+    },
 };
 
 // Notações 
-
 console.log(pessoa.nome);
-console.log(pessoa[sobrenome]); // Forma dinâmica 
+console.log(pessoa['sobrenome']);
+console.log(pessoa.falarNome());
