372f129e73953882ca71efaaff3413d2f61ac697
Gustavo Pedro Lima <gpedr56@gmail.com>
2022-01-06 17:55:00 -0300

    Update Revisando.js

--- JS/JSPOO/Objetos_Protoypes/Revisando.js
+++ JS/JSPOO/Objetos_Protoypes/Revisando.js
@@ -1,4 +1,4 @@
-// FORMA ALTERNATIVA PARA CRIAÇÃO DE OBJETOS: 
+// === FORMA ALTERNATIVA PARA CRIAÇÃO DE OBJETOS: ===
 
 // const pessoa1 = new Object();
 // pessoa1.nome = 'Gustavo';
@@ -16,3 +16,37 @@ const pessoa = {
 console.log(pessoa.nome);
 console.log(pessoa['sobrenome']);
 console.log(pessoa.falarNome());
+
+
+// === "Moldes" para criar objetos ===
+
+//Factory Functions
+function criarPessoa(nome,sobrenome){
+    return{
+        nome,
+        sobrenome,
+        get nomeCompleto(){
+            return `${this.nome}  ${this.sobrenome}`
+        }
+    }
+}
+
+//Constructor Functions
+
+function Pessoa(nome,sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+};
+
+let p1 = new Pessoa('Gustavo','Pedro');
+
+
+//Object.freeze() --> trava o objeto
+
+
+//Object.defineProperty(this, 'estoque',{ --> Basicamente usado para criar valores dentro do objeto, mas de forma mais segura
+//    enumerable: true, --> Mostra a chave
+//    value: estoque, --> É o valor
+//    writable:false, --> Pode alterar?
+//    configurable:true --> Pode reconfigurar?
+//})
